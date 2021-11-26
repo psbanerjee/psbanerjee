22a883b6b6effa5da7f267f9c8fb479fc26eb608
psbanerjee <78534016+psbanerjee@users.noreply.github.com>
2021-11-26 19:52:12 +0530

    Add files via upload

---
+++ my_ubuntu_setup.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+sudo apt update
+sudo apt dist-upgrade -y
+sudo apt install vlc -y
+sudo apt install clementine -y
+sudo apt install shotwell -y
+sudo apt install gfortran -y
+sudo apt install gnuplot-qt -y
+sudo apt install budgie-wallpapers -y
+sudo apt install texlive-full -y
+sudo apt install texstudio -y
+sudo apt install gnome-disk-utility -y
+sudo apt install gedit -y
+sudo apt install transmission -y
+sudo apt install telegram-desktop -y
+sudo apt install libreoffice -y
+sudo apt install octave -y
+sudo apt install neofetch -y
+sudo apt install screenfetch -y
+sudo apt install gnome-system-monitor -y
+sudo apt install ubuntu-restricted-extras -y
+sudo apt install openvpn -y
+sudo apt install gnome-tweaks -y
+sudo apt install cmatrix -y
+sudo snap install opera
+sudo snap install brave
+sudo snap install teams
+#Ubuntu only
+sudo apt remove gnome-logs -y
+sudo apt remove gnome-characters -y
+sudo apt remove update-notifier -y
+sudo apt remove yelp -y
+sudo apt remove eog -y
+sudo snap remove snap-store
+#Run these on terminal
+#gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+#gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+#gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 55
+#gsettings set org.gnome.desktop.interface show-battery-percentage true
+#-------------------------------------
+#-------------------------------------
+sudo apt autoremove -y
+neofetch
