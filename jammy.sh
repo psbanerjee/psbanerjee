4f8a9ae3fa8a5c681e30e08ed6dc1c4d3140e028
psbanerjee <78534016+psbanerjee@users.noreply.github.com>
2023-12-22 12:42:33 +0530

    Update jammy.sh

--- jammy.sh
+++ jammy.sh
@@ -1,1 +1,40 @@
-
+#!/bin/sh
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 50
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+sudo apt dist-upgrade -y
+sudo apt install vlc -y
+sudo apt install clementine -y
+sudo apt install shotwell -y
+sudo apt install gfortran -y
+sudo apt install gnuplot-qt -y
+sudo apt install budgie-wallpapers -y
+sudo apt install texlive-full -y
+sudo apt install texstudio -y
+sudo apt install gnome-disk-utility -y
+sudo apt install gedit -y
+sudo apt install transmission -y
+sudo apt install telegram-desktop -y
+sudo apt install libreoffice -y
+sudo apt install octave -y
+sudo apt install neofetch -y
+#sudo apt install screenfetch -y
+sudo apt install gnome-system-monitor -y
+sudo apt install ubuntu-restricted-extras -y
+sudo apt install openvpn -y
+sudo apt install gnome-tweaks -y
+sudo apt install cmatrix -y
+sudo apt install remmina -y
+sudo apt install thunderbird -y
+sudo apt install fish -y
+sudo apt install inkscape -y
+sudo apt install gimp -y
+sudo apt remove yelp -y
+sudo apt remove eog -y
+#-------------------------------------
+#-------------------------------------
+sudo apt autoremove -y
+neofetch
