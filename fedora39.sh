ef8a0e6d75cab0f3055768b0751a80b8c2b92f58
psbanerjee <78534016+psbanerjee@users.noreply.github.com>
2023-12-29 11:21:50 +0530

    Update fedora39.sh

--- fedora39.sh
+++ fedora39.sh
@@ -2,4 +2,6 @@
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 echo "http_proxy="http://proxy61.iitd.ac.in:3128""|sudo tee -a /etc/environment
 echo "https_proxy="http://proxy61.iitd.ac.in:3128""|sudo tee -a /etc/environment
+echo "max_parallel_downloads=10"|sudo tee -a /etc/dnf/dnf.conf
+echo "fastestmirror=True"|sudo tee -a /etc/dnf/dnf.conf
 sudo dnf update -y
