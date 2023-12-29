770beecaa2fc924570799d32ebf63d9434de21de
psbanerjee <78534016+psbanerjee@users.noreply.github.com>
2023-12-29 11:14:52 +0530

    Update fedora39.sh

--- fedora39.sh
+++ fedora39.sh
@@ -2,3 +2,4 @@
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 echo "http_proxy="http://proxy61.iitd.ac.in:3128""|sudo tee -a /etc/environment
 echo "https_proxy="http://proxy61.iitd.ac.in:3128""|sudo tee -a /etc/environment
+sudo dnf update
